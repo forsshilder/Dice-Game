cf8b46c65e6df76475206af0f2135f4bc293dee6
Selahattin <106350515+forsshilder@users.noreply.github.com>
2023-01-06 14:43:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (randomNumber1===0){
 
 var randomNumber1=dice1();
 
-var randomDiceImage = "C:\\Users\\WARMACHINE\\Documents\\Web Development\\Dicee Challenge\\images\\dice"+randomNumber1+".png";
+var randomDiceImage = "images\\dice"+randomNumber1+".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -31,7 +31,7 @@ if (randomNumber2===0){
 
 var randomNumber2=dice2();
 
-var randomDiceImage2="C:\\Users\\WARMACHINE\\Documents\\Web Development\\Dicee Challenge\\images\\dice"+randomNumber2+".png";
+var randomDiceImage2="images\\dice"+randomNumber2+".png";
 
 var image2 = document.querySelectorAll("img")[1];
 
@@ -39,10 +39,10 @@ image2.setAttribute("src",randomDiceImage2);
 
 
 if(randomDiceImage>randomDiceImage2){
-  document.querySelector("h1").innerHTML="Play 1 Wins!";
+  document.querySelector("h1").innerHTML="🚩Play 1 Wins!";
 }
  else if (randomDiceImage2>randomDiceImage) {
-   document.querySelector("h1").innerHTML="Play 2 Wins";
+   document.querySelector("h1").innerHTML="Play 2 Wins🚩";
  }
 
 else {document.querySelector("h1").innerHTML="Draw!";
